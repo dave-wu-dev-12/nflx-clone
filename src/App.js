5e3cbad663d3c58444c0515e5bb70d2e69f7d723
d wu <dwu@ds-MacBook-Pro.local>
2020-08-13 23:54:39 -0500

    adding more rows

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import inst from './axios';
-import reqs from './request';
-import Row from './Row'
+import React, { useState, useEffect } from "react";
+import reqs from "./request";
+import Row from "./Row";
+import "./App.css";
 
 function App() {
-  const [trendingShows, setTrendingShows] = useState([]);
-  const [NFLXShows, setNFLXShows] = useState([]);
-
-  useEffect(()=> {
-    getAllsShowRows();
-  }, []);
-
-  const getAllsShowRows = async () => {
-    const nflxRes = await inst.get(reqs.getNLFXOGs);
-    setNFLXShows(nflxRes.data.results);
-
-    const trendingRes = await inst.get(reqs.getTrending);
-    setTrendingShows(trendingRes.data.results);
-  }
-
   return (
     <div className="App">
-      <Row title="NETFLIX Originals" shows={NFLXShows}/>
-      <Row title="Trending Now" shows={trendingShows}/>
+      <Row title="NETFLIX Originals" reqURL={reqs.getNLFXOGs} />
+      <Row title="Trending Now" reqURL={reqs.getTrending} useBackdrop="true" />
+      <Row title="Top Rated" reqURL={reqs.getTopRated} useBackdrop="true" />
+      <Row
+        title="Action Movies"
+        reqURL={reqs.getActiMovies}
+        useBackdrop="true"
+      />
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -1,18 +1,37 @@
-import React from 'react';
-import './Row.css';
+import React, { useState, useEffect } from "react";
+import inst from "./axios";
+import "./Row.css";
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-function Row({title, shows}) {
-    return (
-        <div className="row">
-            <h3 className="row__title">{title}</h3>
-            <div className="row__shows">
-                {shows.map(show => (
-                    <img className="row__image" src={base_url + show.poster_path} alt={show.name}/>
-                ))}
-            </div>
-        </div>
-    )
+function Row({ title, reqURL, useBackdrop }) {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    getMovieData(reqURL);
+  }, [reqURL]);
+
+  const getMovieData = async (reqURL) => {
+    const respData = await inst.get(reqURL);
+    setMovies(respData.data.results);
+  };
+
+  return (
+    <div className="row">
+      <h3 className="row__title">{title}</h3>
+      <div className="row__movies">
+        {movies.map((mov) => (
+          <img
+            key={mov.id}
+            className="row__image"
+            src={`${base_url}${
+              useBackdrop ? mov.backdrop_path : mov.poster_path
+            }`}
+            alt={mov.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Row
+export default Row;
